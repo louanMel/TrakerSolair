ed7dda767da4ad6f45e393583d56d9f8c4863570
louanMel <163991271+louanMel@users.noreply.github.com>
2024-03-25 15:12:40 +0100

    az

--- python/main.py
+++ python/main.py
@@ -1,1 +1,11 @@
-print('Hello')
+# Programme permettant la gestion d'un servomoteur
+
+# from machine import Pin,PWM
+
+def initservo(servo,angle):
+    servo.freq(50)
+    position(servo,angle)          # Fonction d'initialisation du servomoteur
+
+def position(servo,angle):         # Fonction permettant de positionner le servomoteur
+    temps_haut = -35*angle+8000
+    servo.duty_u16(temps_haut) 
