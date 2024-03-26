a4145420f30f810b07f28a300082b535b821d685
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-03-26 11:48:29 +0100

    a

--- python/test.py
+++ python/test.py
@@ -2,11 +2,14 @@ from machine import Pin, PWM
 from servomoteur import *
 import time
 
-servo_h = PWM(Pin(25))
-initservo(servo_h, 0)
 servo_v = PWM(Pin(15))
 initservo(servo_v, 0)
+servo_h = PWM(Pin(25))
+initservo(servo_h, 0)
 
 
-position(servo_h, 45)
-position(servo_v, 45)
+for angle in range(10, 150, 1):  # Test de chaque angle
+    position(servo_h, angle)
+    position(servo_v, angle)
+    print("Angle:", angle)
+    time.sleep(0.5)
