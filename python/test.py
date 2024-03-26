4d8d289d7205a67e0ee29b2b773ddc5bf2749c6e
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-03-26 11:37:39 +0100

    a

--- python/test.py
+++ python/test.py
@@ -1,28 +1,12 @@
 from machine import Pin, PWM
+from servomoteur import *
 import time
 
-def set_angle(servo_pwm, angle):
-    print('func')
-    duty = int(-35*angle+8000)
-    print('duty')
-    servo_pwm.duty_u16(duty)
-    print('func fini')
-
-servo_pin = Pin(15)
-servo_pwm = PWM(servo_pin)
+servo_h = PWM(Pin(25))
+initservo(servo_h, 0)
+servo_v = PWM(Pin(15))
+initservo(servo_v, 0)
 
 
-servo_pwm.freq(50)
-print('freq')
-
-set_angle(servo_pwm, 0)
-time.sleep(0.5)
-print('fini1')
-set_angle(servo_pwm, 20)
-time.sleep(0.5)
-print('fini2')
-set_angle(servo_pwm, 10)
-time.sleep(0.5)
-print('fini3')
-
-print('fini')
+position(servo_h, 45)
+position(servo_v, 45)
